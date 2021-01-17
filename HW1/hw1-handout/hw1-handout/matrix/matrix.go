697d429923ccbb08e762c0d77a2d9c69020e53da
ljy68623 <56326180+ljy68623@users.noreply.github.com>
2021-01-16 17:54:09 -0800

    modify matrix.go

--- HW1/hw1-handout/hw1-handout/matrix/matrix.go
+++ HW1/hw1-handout/hw1-handout/matrix/matrix.go
@@ -4,16 +4,58 @@ package matrix
 
 // AreAdjacent returns true iff a and b are adjacent in lst.
 func AreAdjacent(lst []int, a, b int) bool {
-	panic("TODO: implement this!")
+	for i := 0; i < len(lst); i++ {
+		if lst[i] == a {
+			if i-1 >= 0 && lst[i-1] == b {
+				return true
+			} else if i+1 < len(lst) && lst[i+1] == b {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 // Transpose returns the transpose of the 2D matrix mat.
 func Transpose(mat [][]int) [][]int {
-	panic("TODO: implement this!")
+	row := len(mat)
+	if row == 0 {
+		return mat
+	}
+	col := len(mat[0])
+	if col == 0 {
+		return mat
+	}
+	res := make([][]int, col)
+	for i := 0; i < col; i++ {
+		res[i] = make([]int, row)
+	}
+	for i := 0; i < col; i++ {
+		for j := 0; j < row; j++ {
+			res[i][j] = mat[j][i]
+		}
+	}
+	return res
 }
 
 // AreNeighbors returns true iff a and b are Manhattan neighbors in the 2D
 // matrix mat.
 func AreNeighbors(mat [][]int, a, b int) bool {
-	panic("TODO: implement this!")
+	for i := 0; i < len(mat); i++ {
+		for j := 0; j < len(mat[i]); j++ {
+			if mat[i][j] == a {
+				if i-1 >= 0 && mat[i-1][j] == b {
+					return true
+				} else if i+1 < len(mat) && mat[i+1][j] == b {
+					return true
+				} else if j-1 >= 0 && mat[i][j-1] == b {
+					return true
+				} else if j+1 < len(mat[i]) && mat[i][j+1] == b{
+					return true
+				}
+			}
+		}
+	}
+	return false
 }
+
