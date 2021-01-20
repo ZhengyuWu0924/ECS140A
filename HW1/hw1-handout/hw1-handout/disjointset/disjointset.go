b60d32b873fa9fc5be914a90dc9d2f3b4b032afb
Ron~ <72838558+XingyuPan96@users.noreply.github.com>
2021-01-19 21:58:36 -0800

    Update disjointset

--- HW1/hw1-handout/hw1-handout/disjointset/disjointset.go
+++ HW1/hw1-handout/hw1-handout/disjointset/disjointset.go
@@ -12,8 +12,42 @@ type DisjointSet interface {
 }
 
 // TODO: implement a type that satisfies the DisjointSet interface.
+type Set struct {
+	mValueParent map[int]int
+	mValueRank   map[int]int
+}
 
 // NewDisjointSet creates a struct of a type that satisfies the DisjointSet interface.
 func NewDisjointSet() DisjointSet {
-	panic("TODO: implement this!")
+	s := Set{make(map[int]int), make(map[int]int)}
+	return s
+}
+
+func (s Set) FindSet(a int) int {
+	if elem, ok := s.mValueParent[a]; ok == true {
+		if elem != a {
+			s.mValueParent[a] = s.FindSet(elem)
+			return s.mValueParent[a]
+		}
+		return a
+	}
+	s.mValueParent[a] = a
+	s.mValueRank[a] = 0
+	return a
+}
+
+func (s Set) UnionSet(a int, b int) int {
+	a = s.FindSet(a)
+	b = s.FindSet(b)
+	if a == b {
+		return a
+	}
+	if s.mValueRank[a] < s.mValueRank[b] {
+		a, b = b, a
+	}
+	s.mValueParent[b] = a
+	if s.mValueRank[a] == s.mValueRank[b] {
+		s.mValueRank[a]++
+	}
+	return a
 }
