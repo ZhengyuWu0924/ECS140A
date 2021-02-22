fd648cb4592e073b0db9503819c21066e17121c5
ljy68623 <56326180+ljy68623@users.noreply.github.com>
2021-02-22 00:29:40 -0800

    complete +* in eval

--- HW3/hw3-handout/sexpr/eval.go
+++ HW3/hw3-handout/sexpr/eval.go
@@ -2,7 +2,7 @@ package sexpr
 
 import (
 	"errors"
-	//"math/big" // You will need to use this package in your implementation.
+	"math/big" // You will need to use this package in your implementation.
 )
 
 // ErrEval is the error value returned by the Evaluator if the contains
@@ -12,5 +12,107 @@ import (
 var ErrEval = errors.New("eval error")
 
 func (expr *SExpr) Eval() (*SExpr, error) {
-	panic("TODO: implement Eval")
+	if expr == nil {
+		return nil, ErrEval
+	}
+	if expr.atom.typ == tokenNumber {
+		if expr.cdr == nil {
+			return expr, nil
+		} else {
+			tok := &token{typ: tokenNumber, num: expr.atom.num}
+			return &SExpr{atom: tok, car: nil, cdr: nil}, nil
+		}
+	}
+	if expr.atom.typ == tokenSymbol {
+		if expr.atom.literal == "+" {
+			//(+)
+			if expr.cdr != nil {
+				total := big.NewInt(0)
+				if expr.car.isAtom() == false {
+					res,err := expr.car.Eval() 
+					if err != nil || res.atom.typ != tokenNumber {
+						return nil, err
+					}
+					total.Add(total, res.atom.num)
+				} else {					
+					newExpr := expr.cdr
+					for newExpr != nil {
+						if newExpr.atom == nil {
+							break;
+						}
+						res,err := newExpr.Eval() 
+						if err != nil || res.atom.typ != tokenNumber {
+							return nil, err
+						}
+						total.Add(total, res.atom.num)
+						newExpr = newExpr.cdr
+					}
+				}
+				tok := &token{typ: tokenNumber, num: total}
+				return &SExpr{atom: tok, car: nil, cdr: nil}, nil
+			} else {
+				//+
+				return nil, ErrEval
+			}
+		}
+		if expr.atom.literal == "*" {
+			//(+)
+			if expr.cdr != nil {
+				total := big.NewInt(1)
+				if expr.car.isAtom() == false {
+					res,err := expr.car.Eval() 
+					if err != nil || res.atom.typ != tokenNumber {
+						return nil, err
+					}
+					total.Mul(total, res.atom.num)
+				} else {					
+					newExpr := expr.cdr
+					for newExpr != nil {
+						if newExpr.atom == nil {
+							break;
+						}
+						res,err := newExpr.Eval() 
+						if err != nil || res.atom.typ != tokenNumber {
+							return nil, err
+						}
+						total.Mul(total, res.atom.num)
+						newExpr = newExpr.cdr
+					}
+				}
+				tok := &token{typ: tokenNumber, num: total}
+				return &SExpr{atom: tok, car: nil, cdr: nil}, nil
+			} else {
+				//+
+				return nil, ErrEval
+			}
+		}
+		if expr.atom.literal == "CAR" {
+
+		}
+		if expr.atom.literal == "CDR" {
+
+		}
+		if expr.atom.literal == "ATOM" {
+
+		}
+		if expr.atom.literal == "LISTP" {
+
+		}
+		if expr.atom.literal == "LENGTH" {
+			
+		}
+		if expr.atom.literal == "CONS" {
+			
+		}
+		if expr.atom.literal == "QUOTE" {
+			
+		}
+		if expr.atom.literal == "ZEROP" {
+			//
+		}
+	} else {
+
+	}
+	return expr.car.Eval()
+	return expr, nil
 }
