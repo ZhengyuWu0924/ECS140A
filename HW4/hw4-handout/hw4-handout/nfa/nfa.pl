e6b10f2532d7db5ba834a6966f80d54eed501e43
ZhengyuWu0924 <zywwu@ucdavis.edu>
2021-03-06 20:18:45 -0800

    Update nfa.pl

--- HW4/hw4-handout/hw4-handout/nfa/nfa.pl
+++ HW4/hw4-handout/hw4-handout/nfa/nfa.pl
@@ -1,50 +1,19 @@
-isFinalState(CurrentState, FinalState) :- CurrentState == FinalState.
-isEmpty([]).
-inputIsEmpty([]).
+split([H|T], H, T).
 
-nextSym([H|_], H).
-nextSym([_|T], NextSym) :- nextSym(T, NextSym).
+not_reachable(_, State, DifferentState, []) :- State \= DifferentState.
+not_reachable(Nfa, StartState, FinalState, Input) :-
+    split(Input, Symbol, RestInput),
+    transition(Nfa, StartState, Symbol, StateList),
+    not_reachable_loop(Nfa, StateList, FinalState, RestInput)
+.
 
-nextState([H|_], H]).
-nextState([_|T], NextState) :- nextState(T, nextState).
+not_reachable_loop(_, [], _, _) :- true.
+not_reachable_loop(Nfa, StateList, FinalState, Input) :-
+    split(StateList, FirstState, RestStateList),
+    not_reachable(Nfa, FirstState, FinalState, Input),
+    not_reachable_loop(Nfa, RestStateList, FinalState, Input)
+.
 
-nextStateInList([H|_], NextOne) :- NextOne = H.
-nextStateInList([_|T], NextOne) :- nextStateInList(T, NextOne).
-popFirst([_|T], Poped) :- Poped = T.
-
-innerStateCheck([H|_], Target) :- H is Target.
-innerStateCheck([_|T], Target) :- innerStateCheck(T, Target).
-
-isEmptyTransition(Nfa, State, Sym) :- transition(Nfa, State, Sym, List), isEmpty(List).
-isInnerTransition(Nfa, State, Sym) :- transition(Nfa, State, Sym, List), innerStateCheck(List, State).
-
-noOutterTransition(Nfa, State, [H|_]) :- isEmptyTransition(Nfa, State, H).
-noOutterTransition(Nfa, State, [_|T]) :- noOutterTransition(Nfa, State, T).
-noOutterTransition(Nfa, State, [H|_]) :- isInnerTransition(Nfa, State, H).
-noOutterTransition(Nfa, State, [_|T]) :- noOutterTransition(Nfa, State, T).
-
-
-reachable(Nfa, StartState, FinalState, Input) :- 
-    inputIsEmpty(Input),
-    isFinalState(StartState, FinalState).
 reachable(Nfa, StartState, FinalState, Input) :-
-    nextSym(Input, NextSymbol),
-    noOutterTransition(Nfa, StartState, NextSymbol),
-    isFinalState(StartState, FinalState).
-reachable(Nfa, StartState, FinalState, Input) :-
-    nextSym(Input, NextSymbol),
-    transition(Nfa, StartState, NextSymbol, StateList), 
-    reachable(Nfa, nextStateInList(StateList, NextOnes), FinalState, popFirst(Input, Res)).
-
-reachable(Nfa, StaretState, FinalState, Input) :-
-    nextSym(Input, NextSymbol),
-    popFirst(Input, Res),
-    inputIsEmpty(Res),
-    transition(Nfa, StartState, NextSymbol, StateList),
-    nextStateInList(StateList, NextOnes),
-    isFinalState(NextOnes, FinalState).
-reachable(Nfa, StaretState, FinalState, Input) :-
-    nextSym(Input, NextSymbol),
-    transition(Nfa, StartState, NextSymbol, StateList),
-    nextStateInList(StateList, NextOnes),
-    isFinalState(NextOnes, FinalState).
+    not(not_reachable(Nfa, StartState, FinalState, Input))
+.
