d722ca6a8cd854b672f8da439988e36460b00d7e
ljy68623 <56326180+ljy68623@users.noreply.github.com>
2021-03-15 16:38:31 -0700

    updated bug1.go

--- HW5/hw5-handout/bug1/bug1.go
+++ HW5/hw5-handout/bug1/bug1.go
@@ -1,11 +1,19 @@
 package bug1
 
+import "sync"
+
 // Counter stores a count.
 type Counter struct {
 	n int64
 }
 
+
+var mu sync.Mutex
+
 // Inc increments the count in the Counter.
 func (c *Counter) Inc() {
+	mu.Lock()
 	c.n++
+	mu.Unlock()
 }
+
